5f12fab774ab7a7752e0fef732dea41f02e070d7
Zaid Ahmed Ghazi <za2443963@gmail.com>
2024-02-27 15:38:01 +0500

    crud

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.send('Home')
 })
+
+app.listen(port, () => {
+  console.log(`hello world ${port}`)
+})
+
+
+
+// mongodb+srv://zaidahmedghazi:<password>@cluster0.nj0cgcn.mongodb.net/
