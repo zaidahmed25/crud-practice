7ccd0f753ede4b7ba4c0116f30bbca9b77f359df
Zaid Ahmed Ghazi <za2443963@gmail.com>
2024-02-27 14:58:29 +0500

    crud

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-const express = require('express');
-require('dotenv').config()
-
+const express = require('express')
 const app = express()
 
-
 app.get('/', (req, res) => {
-    res.send('<h1>hello world</h1>')
-})
-
-app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${process.env.PORT}`)
+  res.send('hello world')
 })
